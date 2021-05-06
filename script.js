ff893009fe36ce3f7a558006c60f2c187e6d4019
Uli Dell’Amico <ulidellamico@MacBook-Air-de-Uli.local>
2021-05-06 17:03:20 +0200

    added style in js and hiddden secret number

--- script.js
+++ script.js
@@ -10,25 +10,41 @@ document.querySelector('.guess').value = 34;
 console.log(document.querySelector('.guess').value); */
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
-document.querySelector('.number').textContent = secretNumber;
+
 //add an addEventListener
 document.querySelector('.check').addEventListener('click', function() {
 	//console.log(document.querySelector('.guess').value);
 	const guess = Number(document.querySelector('.guess').value);
 	console.log(guess, typeof guess); //IT's s tring for this reason we need to changed it put Number after queryselector.
+	// WHEN PLAYERS DON'T INSERT THE NUMBER
 	if (!guess) {
 		document.querySelector('.message').textContent = '⛔ Not a number ... insert a number';
-		//BOOLEAN
+		//WHEN PALYERS WIN
 	} else if (guess === secretNumber) {
 		document.querySelector('.message').textContent = '🎉 You rock!🎉';
+		document.querySelector('.number').textContent = secretNumber;
+		document.querySelector('body').style.backgroundColor = '#60b347';
+		document.querySelector('.number').style.width = '30rem';
+		// WHEN THE NUMBER IS TOO HIGH
 	} else if (guess > secretNumber) {
-		document.querySelector('.message').textContent = '📈 Too heigh ... try again';
-		score--;
-		document.querySelector('.score').textContent = score;
+		if (score > 1) {
+			document.querySelector('.message').textContent = '📈 Too heigh ... try again';
+			score--;
+			document.querySelector('.score').textContent = score;
+		} else {
+			document.querySelector('.message').textContent = '💣 You lost the game';
+			document.querySelector('.score').textContent = 0;
+		}
+		//WHEN TE NUMBER IS TOO LOW
 	} else if (guess < secretNumber) {
-		document.querySelector('.message').textContent = '📉 Too low ... try again';
-		score--;
-		document.querySelector('.score').textContent = score;
+		if (score > 1) {
+			document.querySelector('.message').textContent = '📉 Too low ... try again';
+			score--;
+			document.querySelector('.score').textContent = score;
+		} else {
+			document.querySelector('.message').textContent = '💣 You lost the game';
+			document.querySelector('.score').textContent = 0;
+		}
 	}
 });
 //LOGIC SECRET NUMBER
@@ -39,3 +55,5 @@ document.querySelector('.check').addEventListener('click', function() {
 //First step: to creat a variable score
 // put score-- to decrease
 //to pint we have to added document.querySelector('.score').textContent = score see line 26
+
+//LOGIC SCORE === 0 YOU LOOS THE GAME
