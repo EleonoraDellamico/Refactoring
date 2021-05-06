222c5bd5eb7f023e9f1c8c95c43f77e5e819b72c
Uli Dell’Amico <ulidellamico@MacBook-Air-de-Uli.local>
2021-05-06 15:38:12 +0200

    Added Handling click

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
 'use strict';
-console.log(document.querySelector('.message').textContent);
-document.querySelector('.message').textContent = 'eli lo vas a conseguir';
+
+//Examples of DOM MANIPULATION
+/* console.log(document.querySelector('.message').textContent); //reading the content
+document.querySelector('.message').textContent = 'eli lo vas a conseguir'; // changed the text it's a dom manipulation
+document.querySelector('.number').textContent = 13;
+document.querySelector('.score').textContent = 20;
+
+document.querySelector('.guess').value = 34;
+console.log(document.querySelector('.guess').value); */
+
+//add an addEventListener
+document.querySelector('.check').addEventListener('click', function() {
+	//console.log(document.querySelector('.guess').value);
+	const guess = Number(document.querySelector('.guess').value);
+	console.log(guess, typeof guess); //IT's s tring for this reason we need to changed it put Number after queryselector.
+	//LOGIC
+	if (!guess) {
+		document.querySelector('.message').textContent = '⛔ Not a number';
+		//BOOLEAN
+	}
+});
